0367c684b1580f776213bcb5611ca6cb35905dc3
David Ye <david.ye@optus.com.au>
2019-02-23 10:56:13 +1100

    1. create `Contact` model 2. create `contacts.json` file by given url's response 3. add one unit test to test `Contact` model

---
+++ Favourite Contact/Module/Contact/Model/Contact.swift
@@ -0,0 +1,30 @@
+//
+//  Contact.swift
+//  Favourite Contact
+//
+//  Created by David Ye on 23/2/19.
+//  Copyright © 2019 David Ye. All rights reserved.
+//
+
+import Foundation
+
+struct Contact: Decodable {
+    let id: Int
+    let firstName: String
+    let lastName: String
+    let email: String
+    let gender: Gender
+    
+    private enum CodingKeys: String, CodingKey {
+        case id
+        case firstName = "first_name"
+        case lastName = "last_name"
+        case email
+        case gender
+    }
+}
+
+enum Gender: String, Decodable {
+    case male = "Male"
+    case female = "Female"
+}
--- Favourite ContactTests/Favourite_ContactTests.swift
+++ Favourite ContactTests/Favourite_ContactTests.swift
@@ -10,24 +10,27 @@ import XCTest
 @testable import Favourite_Contact
 
 class Favourite_ContactTests: XCTestCase {
+    
+    var json: Data?
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
+        guard let url = Bundle(for: type(of: self)).url(forResource: "contacts", withExtension: "json"),
+            let data = try? Data(contentsOf: url) else { return }
+        json = data
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testContact() {
+        do {
+            let contacts = try JSONDecoder().decode([Contact].self, from: json!)
+            XCTAssertNotNil(contacts)
+        } catch {
+            print(error)
+            XCTFail()
         }
     }
 
