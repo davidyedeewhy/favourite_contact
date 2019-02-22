72c42b0a81d7b1b5d851c1e695883f4be287c896
David Ye <david.ye@optus.com.au>
2019-02-23 09:43:48 +1100

    Initial Commit

---
+++ Favourite Contact/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Favourite Contact
+//
+//  Created by David Ye on 23/2/19.
+//  Copyright © 2019 David Ye. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
